16288054f53d67cdddeb5945730cdaab4cecb8b2
gochujjang <rehankurnia@gmail.com>
2023-12-17 16:42:50 +0700

    some changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          add some string
+          hai namamu siapa
         </p>
       </div>
     );
