bacca8f8a512ae15d6158229b8358a69423b9bcb
gochujjang <rehankurnia@gmail.com>
2023-12-26 23:43:56 +0700

    fix tictactoe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import App from "./App";
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
